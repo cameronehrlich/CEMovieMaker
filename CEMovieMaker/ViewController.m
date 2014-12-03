c6d4b6619f9be90b48834593ade89831f1a243c9
cameronehrlich <cameronehrlich@gmail.com>
2014-12-02 16:14:35 -0800

    fixed example. #1

--- CEMovieMaker/ViewController.m
+++ CEMovieMaker/ViewController.m
@@ -38,7 +38,7 @@
     UIImage *icon2 = [UIImage imageNamed:@"icon2"];
     UIImage *icon3 = [UIImage imageNamed:@"icon3"];
     
-    NSDictionary *settings = [CEMovieMaker videoSettingsWithCodec:AVVideoCodecH264 withHeight:icon1.size.width andWidth:icon1.size.height];
+    NSDictionary *settings = [CEMovieMaker videoSettingsWithCodec:AVVideoCodecH264 withWidth:icon1.size.width andHeight:icon1.size.height];
     self.movieMaker = [[CEMovieMaker alloc] initWithSettings:settings];
     for (NSInteger i = 0; i < 10; i++) {
         [frames addObject:icon1];
@@ -46,10 +46,8 @@
         [frames addObject:icon3];
     }
 
-    [self.movieMaker createMovieFromImages:[frames copy] withCompletion:^(BOOL success, NSURL *fileURL){
-        if (success) {
-            [self viewMovieAtUrl:fileURL];
-        }
+    [self.movieMaker createMovieFromImages:[frames copy] withCompletion:^(NSURL *fileURL){
+        [self viewMovieAtUrl:fileURL];
     }];
 }
 
